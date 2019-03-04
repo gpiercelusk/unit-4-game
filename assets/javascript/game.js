9057270e0c53f83acfebec766bc7bd8c4d9b93d4
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-03-03 19:41:05 -0500

    finished the character choices .Added  comments to solidify concepts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-
-
-  //this declares the global variable of characters, their individual attributes and sets the booleans of chosen character and chosen enemy to false 
+  // declares the global variable of characters, their individual attributes and sets the booleans of chosen character and chosen enemy to false 
   var charSelect = [
     {
       name: 'Dash Rendar',
@@ -36,7 +34,7 @@ $(document).ready(function () {
   var playerChosen = false;
   var enemyChosen = false;
 
-  // this function sets up the character button and appends them to the character select area 
+  // initGame function creates the character buttons attcaches their attributes to each button and then appends them to the character select area 
 
   function initGame() {
     $('.gameArea').hide();
@@ -50,12 +48,53 @@ $(document).ready(function () {
       charBtn.append("<p>" + charSelect[i].name + "</p><img src='" + charSelect[i].img + "'class='charImg'><br><p class='health'>Life: " + charSelect[i].health + "</p>");
       $('#charSelect').append(charBtn);
     }
+    // in the same function im appending a new "paragraph" with text 
     var startText = $('<p>');
-    startText.append('Choose your character.');
+    startText.append('Choose your character. Do. Or do not. There is no try.');
     $('#info').append(startText);
   };
+  //function for selecting character one is chosen on click and stays in '.charArea' until loss 
+  //then the rest move to '.enemyArea' 
+  //(this is selecting the charSelect global obj)
 
-
+  $(document).on('click', '.charCard', function () {
+    if (!playerChosen) {
+      //clears previous info
+      $('#info').empty();
+      //brings back the game area
+      $('.gameArea').show();
+      var user = $(this);
+      //add a class user variable
+      user.addClass('user');
+      //appends to yourChar in html
+      $('#yourChar').append(user);
+      playerChosen = true;
+      //appends rest of characters to the enemy area and adds a class to them 
+      $('#enemyArea').append($('#charSelect').children().addClass('enemyChoice'));
+      //changes info text
+      var chooseEnemy = $('<p>');
+      chooseEnemy.append('Choose opponent: I’ve got a bad feeling about this.');
+      $('#info').append(chooseEnemy);
+    }
+  });
+  //click on '#enemyArea' to select a character to then move to '#defendArea' the other 2 images stay in '#enemyArea'
+  $(document).on('click', '.enemyChoice', function () {
+    if (!enemyChosen) {
+      //clears text
+      $('#info').empty();
+      var opponent = $(this);
+      //adds enmyCard class and rmoves enemyChoice class
+      opponent.addClass('enmyCard').removeClass('enemyChoice');
+      // appends 'opponent' to the defender area
+      $('#defArea').append(opponent);
+      enemyChosen = true;
+      //adds new 'p' tag to the info section
+      var atk = $('<p>');
+      atk.append('Use the attack button to fight.')
+      $('#info').append(atk);
+    }
+  });
+  //
 
   // // thomas notes
   // Char(charSelect[0]) {
@@ -69,20 +108,14 @@ $(document).ready(function () {
 
 
 
-  //everything starts/resets to all characters in '.charArea'
-  //
-  // var yourCharacter = $('.charArea')
-  // for (var i = 0; i < charSelect.length; i++) {
-  //   console.log(charSelect)
-  // }
 
 
-  //function for selecting character one is chosen on click and stays in '.charArea' until loss 
-  //then the rest move to '.enemyArea'
 
-  //from here
 
-  //click on '.enemyArea' to select a character to then move to '.defendArea' the other 2 images stay in '.enemyArea'
+
+
+
+
   // if win player must choose another char from the '.enemyArea'
   //if win2 must choose another char from the '.enemyArea'
   //if win3 must choose another char from the '.enemyArea'
